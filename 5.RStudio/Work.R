ee0f0f2819d6b7abe434054e5553b2549b1ea568
HC im <haechan3@naver.com>
2020-09-11 17:27:02 +0900

    2020-09-11

--- 5.RStudio/Work.R
+++ 5.RStudio/Work.R
@@ -1,24 +1,53 @@
 setwd("c:/Rdata") # C드라이브 Rdata파일내
 library(dplyr)    # dplyr패키지 로드
-data <- read.csv("Project.csv")  # winner팀의 winnerDrink.csv파일을 불러옴
+data <- read.csv("Project.csv")  # Project.csv파일을 불러옴
 head(data)       # 불러온 데이터 확인
-str(data)        # 데이터는 12개의 변수와 1038개의 object로 구성되어 있음
+str(data)        
                  
 View(data)       # 데이터 보기
 
 # 각각의 히스토그램
-hist(data$Dow)
+library(car)
+boxplot(data$Nas,horizontal = T)
+shapiro.test(data$Nas)
+tt=powerTransform(data$Nas)
+kk=data$Nas^tt
+hist(kk)
+str(data)
+cor(data)
+mean(data$Nas)
+sd(data$Nas)
+IQR(data$Nas)
+var(data$Nas)
+summary(data$Nas)
 
+kk=data$Dow
+IQR(data$Dow)
+
+boxplot(data$Nas)$stats
+data$Nas <- ifelse(data$Nas < 3160.780 | data$Nas > 6959.960, NA, data$Nas)
+table(is.na(data$Nas))
+
+boxplot(kk,horizontal = T,col=2)
+shapiro.test(kk)
+Nas_log = log(data$Nas)
+Nas_sqrt = sqrt(data$Nas)
+shapiro.test(Nas_log)
+shapiro.test(Nas_sqrt)
+data_z = transform(scale(data))
+head(data_z)
+View(data_z)
+hist(data_z$Dow)
 # 정규성 테스트
-shapiro.test(data$Dow) # p-value = 2.936e-05
+shapiro.test(data$Nas) # p-value = 2.936e-05
 # -> 이를통해 유의확률 p-value가 어느정도 인지 판별할수 있음.
 
 # 상관관계 분석을 위해 필요한 값들만 다시 저장
 #data=data[,-11:-19]
-#View(data)
 
 # 판매량 상관관계 분석
 cor(data)
+cor(data_z)
 # -> S.P500, silver가 양의상관관계, CNY, EUR, JPY, USD, 변동률이 음의 상관관계임
 
 # 데이터에 대해 회귀분석을 적용
@@ -27,6 +56,9 @@ out=lm(Dow~.,data=data)
 # 데이터의 회귀분석 그래프
 plot(out)
 
+plot(data$Dow, data$S.P)
+plot(data$Dow, data$USD)
+plot(data$Dow, data$JYP)
 library(car)     # car패키지 로드
 vif(out)         # 다중공선성 확인
 
